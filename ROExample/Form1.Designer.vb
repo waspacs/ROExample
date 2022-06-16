11bd5812cd0ac7dd72b24208a74e382faf6e2d93
waspacs <107611634+waspacs@users.noreply.github.com>
2022-06-16 11:17:47 +0300

    Add files via upload

---
+++ ROExample/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.readCb = New System.Windows.Forms.CheckBox()
+        Me.hplabel = New System.Windows.Forms.Label()
+        Me.alwaysOnTop = New System.Windows.Forms.CheckBox()
+        Me.readTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'readCb
+        '
+        Me.readCb.AutoSize = True
+        Me.readCb.Location = New System.Drawing.Point(12, 35)
+        Me.readCb.Name = "readCb"
+        Me.readCb.Size = New System.Drawing.Size(78, 17)
+        Me.readCb.TabIndex = 0
+        Me.readCb.Text = "Read Data"
+        Me.readCb.UseVisualStyleBackColor = True
+        '
+        'hplabel
+        '
+        Me.hplabel.AutoSize = True
+        Me.hplabel.Location = New System.Drawing.Point(9, 55)
+        Me.hplabel.Name = "hplabel"
+        Me.hplabel.Size = New System.Drawing.Size(28, 13)
+        Me.hplabel.TabIndex = 1
+        Me.hplabel.Text = "HP: "
+        '
+        'alwaysOnTop
+        '
+        Me.alwaysOnTop.AutoSize = True
+        Me.alwaysOnTop.Location = New System.Drawing.Point(12, 12)
+        Me.alwaysOnTop.Name = "alwaysOnTop"
+        Me.alwaysOnTop.Size = New System.Drawing.Size(98, 17)
+        Me.alwaysOnTop.TabIndex = 2
+        Me.alwaysOnTop.Text = "Always On Top"
+        Me.alwaysOnTop.UseVisualStyleBackColor = True
+        '
+        'readTimer
+        '
+        Me.readTimer.Interval = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(120, 77)
+        Me.Controls.Add(Me.alwaysOnTop)
+        Me.Controls.Add(Me.hplabel)
+        Me.Controls.Add(Me.readCb)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "ROExample"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents readCb As CheckBox
+    Friend WithEvents hplabel As Label
+    Friend WithEvents alwaysOnTop As CheckBox
+    Friend WithEvents readTimer As Timer
+End Class
---
+++ ROExample/Form1.vb
@@ -0,0 +1,64 @@
+﻿Public Class Form1
+    Private Declare Function ReadMemoryLong Lib "kernel32" Alias "ReadProcessMemory" (ByVal Handle As IntPtr, ByVal Address As Long, ByRef Value As Long, Optional ByVal Size As Integer = 8, Optional ByRef Bytes As Integer = 0) As Long
+    Private Declare Function ReadMemoryInteger Lib "kernel32" Alias "ReadProcessMemory" (ByVal Handle As IntPtr, ByVal Address As Long, ByRef Value As Integer, Optional ByVal Size As Integer = 4, Optional ByRef Bytes As Integer = 0) As Integer
+    Private Declare Function WriteMemoryInteger Lib "kernel32" Alias "WriteProcessMemory" (ByVal Handle As IntPtr, ByVal Address As Long, ByRef Value As Integer, Optional ByVal Size As Integer = 4, Optional ByRef Bytes As Integer = 0) As Integer
+
+    Public Function GetPMValue(ByVal ProcessName As String, ByVal Pointer As Long, Optional ByVal OffSets() As Long = Nothing) As Integer
+        Dim ReturnLong As Long = 0
+        Dim ReturnInteger As Integer = 0
+        Dim Pprocess As Process = Process.GetProcessesByName(ProcessName)(1)
+        Dim Phandle As IntPtr = Pprocess.Handle
+        If OffSets IsNot Nothing Then
+            Dim PbaseAdress As Long = Pprocess.MainModule.BaseAddress.ToInt64 + Pointer
+            ReadMemoryLong(Phandle, PbaseAdress, ReturnLong)
+            For Each i As Long In OffSets
+                If i <> OffSets.Last() Then
+                    ReadMemoryLong(Phandle, ReturnLong + i, ReturnLong)
+                End If
+            Next
+            ReadMemoryInteger(Phandle, CLng("&H" + Hex(ReturnLong + OffSets.Last())), ReturnInteger)
+        Else
+            ReadMemoryInteger(Phandle, Pointer, ReturnInteger)
+        End If
+        Return ReturnInteger
+    End Function
+
+    Public Function EditPMValue(ByVal ProcessName As String, ByVal Pointer As Long, ByVal Value As Integer, Optional ByVal OffSets() As Long = Nothing)
+        Dim ReturnLong As Long = 0
+        Dim ReturnInteger As Integer = 0
+        Dim Pprocess As Process = Process.GetProcessesByName(ProcessName)(0)
+        Dim Phandle As IntPtr = Pprocess.Handle
+        If OffSets IsNot Nothing Then
+            Dim PbaseAdress As Long = Pprocess.MainModule.BaseAddress.ToInt64 + Pointer
+            ReadMemoryLong(Phandle, PbaseAdress, ReturnLong)
+            For Each i As Long In OffSets
+                If i <> OffSets.Last() Then
+                    ReadMemoryLong(Phandle, ReturnLong + i, ReturnLong)
+                End If
+            Next
+            WriteMemoryInteger(Phandle, CLng("&H" + Hex(ReturnLong + OffSets.Last())), Value)
+        Else
+            WriteMemoryInteger(Phandle, Pointer, Value)
+        End If
+        Return vbNull
+    End Function
+    Private Sub alwaysOnTop_CheckedChanged(sender As Object, e As EventArgs) Handles alwaysOnTop.CheckedChanged
+        If alwaysOnTop.Checked Then
+            Me.TopMost = True
+        Else
+            Me.TopMost = False
+        End If
+    End Sub
+
+    Private Sub readCb_CheckedChanged(sender As Object, e As EventArgs) Handles readCb.CheckedChanged
+        If readCb.Checked Then
+            readTimer.Start()
+        Else
+            readTimer.Stop()
+        End If
+    End Sub
+
+    Private Sub readTimer_Tick(sender As Object, e As EventArgs) Handles readTimer.Tick
+        hplabel.Text = "HP: " & GetPMValue("RiseOnline-Win64-Shipping", &H5A32590, {&H8, &H8, &HEB0, &H20, &H78, &H2F8, &H84C})
+    End Sub
+End Class
